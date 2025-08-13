1d6e0d132b8998f7d06d1b35ab94755c30c194a9
tritos-consulting <rechnungen.briefe@gmail.com>
2025-08-13 14:33:26 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Dein Repo heißt tritos-consulting/reisekosten → base exakt so:
+// Dein Repo ist tritos-consulting/reisekosten → base MUSS so sein:
 export default defineConfig({
   plugins: [react()],
   base: "/reisekosten/",
