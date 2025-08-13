1b65c44d37a97c973bfb8dd1a885e8366e606404
tritos-consulting <rechnungen.briefe@gmail.com>
2025-08-13 14:22:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repo = (process.env.GITHUB_REPOSITORY || "").split("/")[1] || "";
-
 export default defineConfig({
   plugins: [react()],
-  // Sehr wichtig für GitHub Pages: Basis-URL = /<repo>/
-  base: `/${repo}/`,
+  // WICHTIG: Repo-Name als Base für GitHub Pages
+  base: "/reisekosten/",
 });
